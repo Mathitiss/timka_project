382107876aec6dd4ee5e88c09fda2060d7af550b
Mathitiss <pc.player@mail.ru>
2026-01-08 22:33:25 +0300

    Classes

--- main.py
+++ main.py
@@ -1,65 +1,54 @@
-import tkinter as tk
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 
-class Main(tk.Tk):
-    def __init__(self): 
-        super().__init__()
+class Main:
+    def __init__(self, r):
+        bg_style = ttk.Style()
+        bg_style.configure('bg.TFrame', background='#2B4F26')
 
-        mainframe = tk.Frame(self)
-        mainframe.pack(fill='both', expand=True)
+        self.mainframe = ttk.Frame(r, style='bg.TFrame')
+        self.mainframe.pack(fill='both', expand=True)
 
-        self.page1 = Page1(mainframe, self)
-        self.page1.pack(fill='both', expand=True)
-
-        self.page2 = Page2(mainframe, self)
-        self.page2.pack(fill='both', expand=True)
-
-        self.page1.tkraise()
-
-
-class Page1(tk.Frame):
-    def __init__(self, parent, controller): 
-        super().__init__(parent)
-        self.controller = controller
-
+        # self.page1.tkraise()
         self.GUI()
         
     def GUI(self):
-        self.controller.title("DeerView")
-        self.controller.geometry("1440x860")
-        self.controller.configure(bg='#2B4F26')
-
         image = Image.open("deer_logo.png") 
         resized_image = image.resize((450, 400))
         img = ImageTk.PhotoImage(resized_image)
 
-        logo = tk.Label(self, image=img, borderwidth=0, highlightthickness=0)
+        logo = ttk.Label(self.mainframe, image=img, background='#2B4F26')
         logo.image = img
         logo.pack()
         
-        text1 = tk.Label(self, text="DeerView", font=("Courier New", 26, "bold"), bg='#2B4F26', fg='white')
+        text1 = ttk.Label(self.mainframe, text="DeerView", font=("Courier New", 26, "bold"), background='#2B4F26', foreground='white')
         text1.pack()
 
-        button = tk.Button(self, text="НАЧАТЬ", command=self.show_page2, width=15, height=1, font=("Courier New", 26, "bold"))
+        btn_style = ttk.Style()
+        btn_style.configure('btn.TButton', width=15, height=1, font=("Courier New", 26, "bold"))
+
+        button = ttk.Button(self.mainframe, text="НАЧАТЬ", command=self.show_page2, style='btn.TButton')
         button.pack(pady=100)
 
-        text2 = tk.Label(self, text="version alpha 1.0", font=("Courier New", 10), bg='#2B4F26', fg='white')
-        text2.pack(side=tk.BOTTOM)
+        text2 = ttk.Label(self.mainframe, text="version alpha 1.0", font=("Courier New", 10), background='#2B4F26', foreground='white')
+        text2.pack(side=BOTTOM)
 
     def show_page2(self):
-        self.controller.page2.tkraise()
+        pass
+        # tkraise()
+
+# class Enter():
+#     def __init__(self): 
+#         self.GUI()
+        
+#     def GUI(self):
+#         pass
 
 
-class Page2(tk.Frame):
-    def __init__(self, parent, controller): 
-        super().__init__(parent)
-        self.GUI()
+root = Tk()
+root.title("DeerView")
+root.geometry("1440x860")
 
-    def GUI(self):
-        text1 = tk.Label(self, text="SECOND PAGE", font=("Courier New", 26, "bold"), bg='#2B4F26', fg='white')
-        text1.pack()
-
-
-app = Main()
-app.mainloop()
+App = Main(root)
+root.mainloop()
